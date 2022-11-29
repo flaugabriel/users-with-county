f9f4be3a16118f28548775081e38b6da7208b4fc
Gabriel Flauzino Mota <gmota@bionexo.com>
2022-11-29 19:41:29 -0400

    done tdd

--- spec/factories.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory(:user) do
-    name { Faker::Internet.name }
-    email { Faker::Internet.email }
-    cpf { Faker::Internet.email }
-    phone { Faker::Internet.email }
-    birth_date { Faker::Internet.email }
-  end
-end
